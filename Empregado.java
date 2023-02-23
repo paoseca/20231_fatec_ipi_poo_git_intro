a98c4a27b7829a52d2ec4ac93e3ad8135ddfa2e8
Renan Moreira <renan.batista7@fatec.sp.gov.br>
2023-02-23 17:26:26 -0300

    Calculo de salario efetuado de acordo com o tipo

--- Empregado.java
+++ Empregado.java
@@ -1,7 +1,21 @@
 public class Empregado {
     //variavel de instancia (objeto). cada objeto tem sua propria copia
     private String nome; // encapsulou variavel nome, outras classes nao conseguem acessar ela, apenas Empregado
+    private int tipo; // representar tipos com variaveis // polimorfismo dinamico
     private int idade;
+    private double salario;
+    private double comissao;
+    private double bonus;
+
+    public double calcularSalario(){
+        if (tipo == 0) 
+            return salario;
+        if (tipo == 1)
+            return salario + salario * comissao;
+        if (tipo == 2)
+            return salario + bonus;
+        return 0;
+    }
 
     public String getNome(){
         return nome;
